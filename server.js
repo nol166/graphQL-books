e55e63ceca04a303bd6da0dcf90b61387ee1da1c
John McCambridge <jmccamb@gmail.com>
2020-02-25 14:43:22 -0600

    create root query type, book type

--- server.js
+++ server.js
@@ -1,22 +1,41 @@
 const express = require('express');
 const expressGraphQL = require('express-graphql');
-const { GraphQLSchema, GraphQLObjectType, GraphQLString } = require('graphql')
-import { authors, books } from './exampleData'
+// import only what we need in terms of types
+const { GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLList, GraphQLInt, GraphQLNonNull } = require('graphql');
+//extract what would be server data into its own file
+const { books, authors } = require("./exampleData")
 
 const app = express();
 
-const schema = new GraphQLSchema({
-    query: new GraphQLObjectType({
-        name: 'HelloWorld',
-        fields: () => ({
-            messsage: {
-                type: GraphQLString,
-                resolve: () => 'Hello world'
-            }
-        }),
+// creating our own book type
+const BookType = new GraphQLObjectType({
+    name: "Book",
+    description: 'This represents a book written by an author',
+    fields: () => ({
+        id: { type: GraphQLNonNull(GraphQLInt) }, // must be a new non nullable type of int
+        name: { type: GraphQLNonNull(GraphQLString) }, // also non nullable type of string
+        authorId: { type: GraphQLNonNull(GraphQLInt) } // 
     })
 })
 
+// root query type
+const RootQueryType = new GraphQLObjectType({
+    name: 'Query',
+    description: 'Root Query',
+    fields: () => ({
+        books: {
+            type: new GraphQLList(BookType),
+            description: 'List of all books',
+            resolve: () => books
+        }
+    })
+})
+
+const schema = new GraphQLSchema({
+    query: RootQueryType
+})
+
+// simple express server
 app.use('/graphql', expressGraphQL({
     schema: schema,
     graphiql: true
